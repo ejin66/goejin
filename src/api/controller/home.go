718caafeedb659bbc8eab6e4755765a0d4ddeb3f
ejin <250022190@qq.com>
2017-07-06 15:37:26 +0800

    framework structure modify

--- src/api/controller/home.go
+++ src/api/controller/home.go
@@ -4,50 +4,44 @@ import (
 	"strconv"
 	"api/system/controller"
 	"api/db"
+	"fmt"
 )
 
 type HomeController struct {
 	controller.BaseController
 }
 
-func (self *HomeController) Instance() controller.Base {
-	return &HomeController{}
-}
-
-
-func (self *HomeController) Filter() (bool,string) {
-	return true,""
-}
-
-
-
+//func (self *HomeController) Filter() (bool,string) {
+//	return true,""
+//}
 
 /*              以下方法为自定义方法             */
 
-func (self *HomeController) Index() {
-	body := self.Ctx.Body()
-	self.Ctx.Response("index....." + body)
+func (this *HomeController) Index() {
+	fmt.Printf("%p,%p",this,&this.BaseController)
+	body := this.Ctx.Body()
+	this.Ctx.Response("index....." + body)
 }
 
-func (self *HomeController) Show() {
-	self.Ctx.Response("show data")
+func (this *HomeController) Show() {
+	this.Ctx.Response("show data")
 }
 
-func (self *HomeController) Add(i string, j string) {
+func (this *HomeController) Add(i string, j string) {
 	ii, err := strconv.Atoi(i)
 	ij, err2 := strconv.Atoi(j)
 
 	if err != nil || err2 != nil {
-		self.Ctx.Response("arguments type error")
+		this.Ctx.Response("arguments type error")
 	}
-	self.Ctx.Response("计算结果：" + strconv.Itoa(ii+ij))
+	this.Ctx.Response("计算结果：" + strconv.Itoa(ii+ij))
 }
 
-func (self *HomeController) Insert(name string) {
-	result := db.Insert("user_info", db.Ipt{"user_name" : name})
+func (this *HomeController) Insert(name string) {
+	result := db.Insert("user_info", db.Ipt{"user_name": name})
 	if result == -1 {
-		self.Ctx.Response("insert failed!")
+		this.Ctx.Response("insert failed!")
 		return
 	}
-	self.Ctx.Response("insert successful! Row number:" + strconv.Itoa(int(result)))
+	this.Ctx.Response("insert successful! Row number:" + strconv.Itoa(int(result)))
 }
