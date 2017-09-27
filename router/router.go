35b971191aa1540db932360bdb9b31ed068506c9
ejin66 <250022190@qq.com>
2017-09-27 08:47:57 +0800

    Update router.go

--- router/router.go
+++ router/router.go
@@ -10,8 +10,5 @@ Route table
 POST/GET/EMPTY STRING(both post and get)
  */
 var RouteTable = sysController.Router{
-	"HOME": sysController.Cfg{&controller.HomeController{}, "", sysController.MethodMap{ "Index":""}},
-	"FILE": sysController.Cfg{Cb: &controller.FileController{}, DefaultMethod: "GET"},
-	"RECIPE": sysController.Cfg{Cb: &controller.RecipeController{}, DefaultMethod: ""},
-	"ARTICLE": sysController.Cfg{&controller.ArticleController{}, "POST",sysController.MethodMap{"Web":"GET"}},
+	"HOME": sysController.Cfg{&controller.HomeController{}, "", sysController.MethodMap{ "Index":""}}
 }
